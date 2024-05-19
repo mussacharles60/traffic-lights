450d56108f9dcc5ba97819c614d4eb7837dff0c8
Mussa Charles <mussacharles60@gmail.com>
2024-05-20 02:17:23 +0300

    Update code.ino

--- code/code.ino
+++ code/code.ino
@@ -41,18 +41,27 @@ int distance_A_3 = 0;
 int distance_B_3 = 0;
 int distance_C_3 = 0;
 
-elapsedMillis alternate_timer;
-elapsedMillis idle_timer;
+elapsedMillis alternate_timer;           // timer alternation for red to green lights
+elapsedMillis idle_timer;                // timer for yellow lights
+elapsedMillis long_traffic_timer;        // timer for "long traffic waiting to cross the road"
 
 uint8_t alternate_count = 0;
 uint8_t idle_count = 0;
+uint8_t long_traffic_count = 0;
 
 bool is_idle_state = false;
 
-uint8_t ALTERNATE_TIMEOUT = 10; // time interval for traffic lights to stay on green or red (seconds)
-uint8_t IDLE_TIMEOUT = 3; // timeout for the yellow lights (seconds)
+uint8_t ALTERNATE_TIMEOUT = 10;          // time interval for traffic lights to stay on green or red (seconds)
+uint8_t IDLE_TIMEOUT = 3;                // timeout for the yellow lights (seconds)
+uint8_t SHORT_DISTANCE_TRAFFIC = 10;     // used to determine if there's long traffic waiting to cross the road
+uint8_t LONG_TRAFFIC_MEASURING_TIME = 3; // for measuring how long the (long traffic are waiting to cross the road)
 
-uint8_t lights_state = 0; // for allowing which traffic lights to turn on
+uint8_t lights_state = 0;                // for allowing which traffic lights to turn on
+
+bool was_interrupted = false;            // for allowing green lights to turn on based on the short distance of the detected side (if there's long traffic on the road)
+bool should_select_distance = true;
+uint8_t distance_to_measure = 0;
+uint8_t selected_distance = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -91,6 +100,7 @@ void setup() {
 
   Serial.println(F("START"));
   delay(1000);
+  // initial state
   lights_state = 1;
   switchLights();
 }
@@ -100,6 +110,7 @@ void loop() {
   readUltrasonicA();
   readUltrasonicB();
   readUltrasonicC();
+  longTrafficDetection();
 }
 
 void readUltrasonicA() {
@@ -125,7 +136,7 @@ void readUltrasonicA() {
   else if (reading_round_A == 2) {
     reading_round_A = 0;
     distance_A_3 = d;
-    int dd = (distance_A_1 + distance_A_2 + distance_A_3) / 3;
+    int dd = (distance_A_1 + distance_A_2 + distance_A_3) / 3; // get average distance readings
     if (dd != distance_A) {
       distance_A = dd;
       Serial.print(F("Distance A: "));
@@ -153,7 +164,7 @@ void readUltrasonicB() {
   else if (reading_round_B == 2) {
     reading_round_B = 0;
     distance_B_3 = d;
-    int dd = (distance_B_1 + distance_B_2 + distance_B_3) / 3;
+    int dd = (distance_B_1 + distance_B_2 + distance_B_3) / 3; // get average distance readings
     if (dd != distance_B) {
       distance_B = dd;
       Serial.print(F("Distance B: "));
@@ -181,7 +192,7 @@ void readUltrasonicC() {
   else if (reading_round_C == 2) {
     reading_round_C = 0;
     distance_C_3 = d;
-    int dd = (distance_C_1 + distance_C_2 + distance_C_3) / 3;
+    int dd = (distance_C_1 + distance_C_2 + distance_C_3) / 3; // get average distance readings
     if (dd != distance_C) {
       distance_C = dd;
       Serial.print(F("Distance C: "));
@@ -200,6 +211,13 @@ void routines() {
       is_idle_state = true;
       idle_timer = 0;
       setIdleState();
+    } else if (alternate_count == ALTERNATE_TIMEOUT / 2) { // through the half way of the routines
+      if (was_interrupted && selected_distance != 0 && !should_select_distance) { // check if it was interrupted by the "long traffic waiting to cross" procedures
+        selected_distance = 0;
+        was_interrupted = false; // quit the interrupted procedures
+        should_select_distance = true; // allow to measure the "long traffic waiting" detection again
+        Serial.println(F("Exit interruption"));
+      }
     }
     idle_timer = 0;
   }
@@ -210,17 +228,33 @@ void routines() {
       idle_count = 0;
       // time to turn off yellow lights
       is_idle_state = false;
-      if (lights_state == 0) {
-        lights_state = 1;
-      } else {
-        lights_state = lights_state + 1;
+      if (was_interrupted && selected_distance == 0) { // normal routines
+        if (lights_state == 0) {
+          lights_state = 1;
+        } else {
+          lights_state = lights_state + 1;
+        }
+        if (lights_state > 3) {
+          lights_state = 1;
+        }
+        alternate_timer = 0;
+        // switch lights
+        switchLights();
       }
-      if (lights_state > 3) {
-        lights_state = 1;
+      else { // interrupted due to long traffic waiting to cross the road
+        if (selected_distance == 1) { // A
+          lights_state = 1;
+        }
+        else if (selected_distance == 2) { // B
+          lights_state = 2;
+        }
+        else if (selected_distance == 3) { // C
+          lights_state = 3;
+        }
+        alternate_timer = 0;
+        // switch lights
+        switchLights();
       }
-      alternate_timer = 0;
-      // switch lights
-      switchLights();
     }
     alternate_timer = 0;
   }
@@ -284,5 +318,86 @@ void switchLights() {
       digitalWrite(GREEN_B, 0);
       digitalWrite(GREEN_C, 1);
       break;
+  }
+}
+
+void longTrafficDetection() {
+  if (should_select_distance) {
+    if (distance_A <= SHORT_DISTANCE_TRAFFIC) {
+      should_select_distance = false;
+      distance_to_measure = 1; // A
+    }
+    else if (distance_B <= SHORT_DISTANCE_TRAFFIC) {
+      should_select_distance = false;
+      distance_to_measure = 2; // B
+    }
+    else if (distance_C <= SHORT_DISTANCE_TRAFFIC) {
+      should_select_distance = false;
+      distance_to_measure = 3; // C
+    }
+    long_traffic_timer = 0;
+  }
+  if (long_traffic_timer > 1000 && 
+    !should_select_distance && 
+    selected_distance == 0 && 
+    distance_to_measure > 0 && !was_interrupted) {
+    long_traffic_timer = 0;
+    long_traffic_count = long_traffic_count + 1;
+    if (long_traffic_count >= LONG_TRAFFIC_MEASURING_TIME) {
+      long_traffic_count = 0;
+      switch (distance_to_measure) {
+        case 1: // A
+          if (distance_A <= SHORT_DISTANCE_TRAFFIC) { // if is still long traffic waiting
+            should_select_distance = false;
+            selected_distance = 1; // A
+            // interrupt now
+            alternate_timer = 0;
+            is_idle_state = false;
+            alternate_count = ALTERNATE_TIMEOUT;
+            // this will cause the program to enter into the idle timer
+            was_interrupted = true;
+            Serial.println(F("Interrupted: A"));
+          } else { // reset
+            should_select_distance = true;
+            selected_distance = 0;
+            was_interrupted = false;
+          }
+          break;
+        case 2: // B
+          if (distance_B <= SHORT_DISTANCE_TRAFFIC) { // if is still long traffic waiting
+            should_select_distance = false;
+            selected_distance = 2; // B
+            // interrupt now
+            alternate_timer = 0;
+            is_idle_state = false;
+            alternate_count = ALTERNATE_TIMEOUT;
+            // this will cause the program to enter into the idle timer
+            was_interrupted = true;
+            Serial.println(F("Interrupted: B"));
+          } else { // reset
+            should_select_distance = true;
+            selected_distance = 0;
+            was_interrupted = false;
+          }
+          break;
+        case 3: // C
+          if (distance_C <= SHORT_DISTANCE_TRAFFIC) { // if is still long traffic waiting
+            should_select_distance = false;
+            selected_distance = 3; // C
+            // interrupt now
+            alternate_timer = 0;
+            is_idle_state = false;
+            alternate_count = ALTERNATE_TIMEOUT;
+            // this will cause the program to enter into the idle timer
+            was_interrupted = true;
+            Serial.println(F("Interrupted: C"));
+          } else { // reset
+            should_select_distance = true;
+            selected_distance = 0;
+            was_interrupted = false;
+          }
+          break;
+      }
+    }
   }
 }
